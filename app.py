328b97ef66287b3edd4a0c503cdb19232d9d2ea2
HnElmostafa kellali <hanane.kellali@gmail.com>
2025-03-22 15:19:17 +0000

    db_url remplaced by the real url

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import environ
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('postgresql://postgres:postgres@flask_db:5432/postgres')
 db = SQLAlchemy(app)
 
 class User(db.Model):
